4a660e02444b9ff4a13a874718b8668ba0f95c91
alyleui <alyleui@gmail.com>
2018-02-20 19:39:10 -0500

    bump version

--- export/src/index.ts
+++ export/src/index.ts
@@ -11,9 +11,11 @@ import { ngc } from './common/ngc';
 import { mergeRecursive } from './common/merge-recursive';
 import { uglifyJsFile } from './common/minify-sources';
 const inlineResources = require('./common/inline-resources');
+const { prettySize } = require('pretty-size');
+const gzipSize = require('gzip-size');
 import { extname } from 'path';
 import { readdir } from 'fs';
-import { writeFileSync, removeSync, copySync, pathExists, pathExistsSync, readFileSync } from 'fs-extra';
+import { writeFileSync, removeSync, copySync, pathExists, pathExistsSync, readFileSync, statSync } from 'fs-extra';
 const ambConfig = require(`${process.cwd()}/.amb.json`);
 const libRoot = ambConfig['root'];
 const tmp = ambConfig['tmp'] = `${process.cwd()}/.tmp/${libRoot.split('/').reverse()[0]}`;
@@ -44,16 +46,18 @@ const checkPath = (path: string) => {
 /** Check folder libRoot */
 checkPath(libRoot);
 
-function getPkg(module: Module) {
-  /** Verifying its existence {root}/{dir}/package.json */
-  if (module.pkg) {
-    module.pkg = `${tmp}/${module.dir}/${module.pkg}/package.json`;
-  } else if (pathExistsSync(`${tmp}/${module.dir}/package.json`)) {
-    module.pkg = `${tmp}/${module.dir}/package.json`;
+function getModuleFile(module: Module, key: string, file: string) {
+  let moduleFile = (module as any)[key];
+
+  /** Verifying its existence file default {root}/{dir}/${file} */
+  if (pathExistsSync(`${tmp}/${module.dir}/${file}`)) {
+    moduleFile = `${tmp}/${module.dir}/${file}`;
+  } else if (moduleFile) {
+    moduleFile = `${tmp}/${module.dir}/${moduleFile}/${file}`;
   } else {
-    module.pkg = `${tmp}/package.json`;
+    moduleFile = `${tmp}/${file}`;
   }
-  return module.pkg;
+  return moduleFile;
 }
 
 /** Update Modules */
@@ -67,18 +71,22 @@ modules.forEach((module) => {
   }
   module.outDir = `${process.cwd()}/${ambConfig['outDir']}/${module.dir}`;
 
-  module.pkg = getPkg(module);
+  module.pkg = getModuleFile(module, 'pkg', 'package.json');
 
   if (!module.version) {
     module.version = version;
   }
 
-  if (!module.es2015) {
-    module.es2015 = `tsconfig-es2015.json`;
-  }
-  if (!module.esm) {
-    module.esm = `tsconfig-esm.json`;
-  }
+  // if (!module.es2015) {
+  //   module.es2015 = `tsconfig-build.json`;
+  // }
+  module.es2015 = getModuleFile(module, 'es2015', 'tsconfig-build.json');
+
+  // if (!module.esm) {
+  //   module.esm = `tsconfig-esm.json`;
+  // }
+  module.esm = getModuleFile(module, 'esm', 'tsconfig-esm.json');
+
   for (const $key in module) {
     if (
       module.hasOwnProperty($key) &&
@@ -90,12 +98,14 @@ modules.forEach((module) => {
        * Set absolute routes
        * default to {root}/{dir}/key
        */
-      (module as any)[$key] = `${tmp}/${module.dir}/${moduleConfig}`;
+      // (module as any)[$key] = `${tmp}/${module.dir}/${moduleConfig}`;
     }
   }
-  checkPath(module.pkg);
+  checkPath(`${module.pkg}`);
   module.dir = `${process.cwd()}/${libRoot}/${module.dir}`;
 });
+
+// Console.log('modules: ', JSON.stringify(modules, null, 2));
 
 function writeTsConfig(module: Module): {state: boolean, err: any} {
   const es2015 = {
@@ -157,7 +167,9 @@ function writeTsConfig(module: Module): {state: boolean, err: any} {
       writeFileSync(`${module[type]}`, JSON.stringify(tsconfigContent, null, 2));
     } else {
       tsconfigContent['compilerOptions']['outDir'] = '{dir}';
+      tsconfigContent['angularCompilerOptions']['flatModuleOutFile'] = `index.js`;
       tsconfigContent['angularCompilerOptions']['flatModuleId'] = `${module.name}`;
+      tsconfigContent['files'] = ['public_api.ts'];
       writeFileSync(`${module[type]}`, JSON.stringify(tsconfigContent, null, 2));
     }
   };
@@ -176,66 +188,117 @@ function writeTsConfig(module: Module): {state: boolean, err: any} {
     };
   }
 }
+const converterToModuleName = (str: string) => str
+          .split('/')
+          .filter((val: string) => !!val)
+          .map((part: string) => camelcase(part))
+          .join('.');
+const converterToFileName = (str: string) => str
+          .split('/')
+          .filter((val: string) => !!val)
+          .map((part: string) => (part))
+          .join('__');
 
+const promiseSerial = (funcs: any[]): Promise<any> =>
+  funcs.reduce((promise, func) =>
+    promise.then((result: any) => func().then(Array.prototype.concat.bind(result))),
+    Promise.resolve([]));
 function buildModules():
-Observable<{module: Module, state: 'start' | 'end' | 'err' | 'finish', msg: string}> {
+Observable<{module: Module | null, state: 'start' | 'end' | 'err' | 'finish', msg: string | null}> {
   return new Observable((observer) => {
 
     /** Inline resources */
     (<Promise<void>>inlineResources(tmp))
     .then(() => {
-      modules.forEach((module) => {
-        const state = (st: 'start' | 'end' | 'err' | 'finish', msg = '') => (observer.next({module, state: st, msg}));
-        state('start', 'Updating tsconfigs');
-        writeTsConfig(module).state ? state('end', 'tsconfigs updated') : state('err');
-        state('start');
+      const funcs = modules.map(module => () => {
+        const state = (st: 'start' | 'end' | 'err' | 'finish', msg: string | null = null) => (observer.next({module, state: st, msg}));
+        state('start', 'Updating tsconfig');
+        writeTsConfig(module).state ? state('end', 'tsconfig updated') : state('err');
         const es2015 = () => {
+          state('start', 'Building es2015...');
           return ngc(`${module.es2015}`, `${module.outDir}`)
-          // .then(() => state('end'))
-          .catch(() => state('err'));
+            .then(() => state('end', 'es2015 built'))
+            .catch(() => state('err'));
         };
         const esm = () => {
+          state('start', 'Building esm...');
           return ngc(`${module.esm}`, `${module.outDir}`, 'esm')
-          // .then(() => state('end'))
-          .catch(() => state('err'));
+            .then(() => state('end', 'esm built'))
+            .catch(() => state('err'));
         };
-        Observable.forkJoin(
-          es2015(),
-          esm()
-        )
-        .subscribe(() => {
+        return es2015()
+        .then(() => esm())
+        .then(() => {
           const globals = GLOBALS as any;
           const inputPathJS = `${module.outDir}/index.js`;
-          const outputPathUMD = `${module.outDir}/${module.name}.umd.js`;
-          const outputPathUMDmin = `${module.outDir}/${module.name}.umd.min.js`;
-          generateBundle(inputPathJS, {
+          const outputPathUMD = `${module.outDir}/${converterToFileName(module.name)}.umd.js`;
+          const outputPathUMDmin = `${module.outDir}/${converterToFileName(module.name)}.umd.min.js`;
+          return generateBundle(inputPathJS, {
             file: outputPathUMD,
             globals,
-            name: camelcase(module.name)
+            name: converterToModuleName(module.name)
           })
-          .catch((err: any) => state('err', err))
-          .then(() => {
-            uglifyJsFile(outputPathUMD, outputPathUMDmin);
-            const pkgContent = JSON.parse(readFileSync(`${module.pkg}`, {encoding: 'utf8'}));
-            /** Set Config */
-            pkgContent['name'] = module.name;
-            pkgContent['version'] = module.version;
-            pkgContent['main'] = `${module.name}.umd.js`;
-            pkgContent['es2015'] = `es2015/index.js`;
-            pkgContent['module'] = `index.js`;
-            pkgContent['typings'] = `index.d.js`;
-            writeFileSync(`${module.outDir}/package.json`, JSON.stringify(pkgContent, null, 2));
-            state('end');
-          });
+          .then(() => ({outputPathUMD, outputPathUMDmin}))
+          .catch((err: any) => state('err', err));
+        })
+        .catch((err: any) => state('err', err))
+        .then((result) => {
+          state('start', 'Minifying resources...');
+          uglifyJsFile(result.outputPathUMD, result.outputPathUMDmin);
+          state('end', 'Successfully minified resources');
+          state('start', 'Creating package...');
+          const pkgContent = JSON.parse(readFileSync(`${module.pkg}`, {encoding: 'utf8'}));
+          /** Set Config */
+          pkgContent['name'] = module.name;
+          pkgContent['version'] = module.version;
+          pkgContent['main'] = `${module.name}.umd.js`;
+          pkgContent['es2015'] = `es2015/index.js`;
+          pkgContent['module'] = `index.js`;
+          pkgContent['typings'] = `index.d.js`;
+          writeFileSync(`${module.outDir}/package.json`, JSON.stringify(pkgContent, null, 2));
+          state('end', 'Package created successfully');
+          state('end');
         });
       });
+      promiseSerial(funcs)
+      .then(() => {
+        measure();
+        observer.next({module: null, state: 'finish', msg: null});
+      })
+      .catch(console.error.bind(console));
     });
   });
 }
 
+function measure() {
+  const measureOf = (module: Module, min?: '.min') => {
+    const moduleName = converterToFileName(module.name);
+    const path = `${module.outDir}/${moduleName}.umd${min || ''}.js`;
+    const file = readFileSync(path);
+    const gzip = prettySize(gzipSize.sync(file), true);
+    const size = prettySize(statSync(path).size, true);
+    return { gzip, size };
+  };
+  modules.forEach((module) => {
+    const moduleName = converterToFileName(module.name);
+    const umd = measureOf(module);
+    const umdMin = measureOf(module, '.min');
+    const txt = {
+      original: `${chalk.gray('[original]')}`,
+      gzip: `${chalk.gray('[gzip]')}`
+    };
+    const green = chalk.greenBright;
+    const gray = chalk.gray;
+    console.log(chalk.yellow(
+      `  ${moduleName}.umd.js ${gray('[original]')} ${chalk.red(umd.size)} ${txt.gzip} ${green(umd.gzip)}
+  ${moduleName}.umd.min.js ${gray('[minified]')} ${chalk.red(umdMin.size)} ${txt.gzip} ${green(umdMin.gzip)}`));
+  });
+}
+
 const spinner = ora();
+spinner.color = 'magenta';
 buildModules().subscribe((state) => {
-  const name = chalk.hex('#304ffe')(`${camelcase(state.module.name)}: `);
+  const name = state.module ? chalk.hex('#304ffe')(`${camelcase(state.module.name)}: `) : '';
   if (state.state === 'start') {
     state.msg ?
               spinner.start(`${name}${state.msg}`) :
@@ -250,7 +313,7 @@ buildModules().subscribe((state) => {
     spinner.stop();
     Console.error('~Failed compilation', state.msg ? `=> ${state.msg}` : '');
   } else if (state.state === 'finish') {
-    clean();
+    removeSync(`${process.cwd()}/.tmp`);
     spinner.stop();
     Console.log('~All the compilation has been successful');
   }
